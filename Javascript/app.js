af7b99b4a99d867816b316b57a8e44ae29093fd6
Danny Cando <danny.cando@upec.edu.ec>
2020-06-30 11:06:32 -0500

    Ejercio web

---
+++ Javascript/app.js
@@ -0,0 +1,50 @@
+/*let passwordDB= 'pepe123'
+
+let input ='pepe123'
+
+let result = input == passwordDB;
+
+if (result == true){
+    console.log('login Correcto');
+} else {
+    console.log('Contraseña incorrecto')
+}*/
+/*let score = 70;
+
+if(score > 30){
+    console.log('You need to prctice more');
+}else if(score > 15){
+    console.log('Estas mejorando');
+
+}
+    else{
+        console.log('Tou need to follow this tutorial');
+    }*/
+/*let typeCard = 'asdsdsa';
+
+switch(typeCard){
+    case 'Debid Card':
+        console.log('This is a debid card');
+        break;
+    case 'Credit Card':
+            console.log('This is a credit card');
+        break;
+    default:
+        console.log('No Card');
+}*/
+/*let count = 1;
+
+while(count <= 50){
+    console.log(count);
+    count++;
+}*/
+/*let names = ['danny','fabian','daniel'];
+
+for(let i = names.length - 1; i > 0; i--)
+{ 
+    console.log(names[i]);
+}*/
+function greeting(){
+    console.log('Hello');
+}
+greeting();  
